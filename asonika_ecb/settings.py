ad95f482f076a1dd3d2387c20a77775e32fabed2
Kolesov Daniil <d.kolesov@tinkoff.ru>
2021-07-02 17:54:22 +0300

    Use postgres as default database
    https://www.notion.so/PostgreSQL-c5f72c688efe4b6d8af614aaae89d63a

--- asonika_ecb/settings.py
+++ asonika_ecb/settings.py
@@ -79,7 +79,14 @@ WSGI_APPLICATION = 'asonika_ecb.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD', default=''),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT', default=''),
+    }
 }
 
 
