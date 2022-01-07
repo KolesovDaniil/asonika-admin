2590b3ab0f33b8d6e3ee58a819942c8cf871668f
Kolesov Daniil <d.kolesov@tinkoff.ru>
2022-01-07 23:06:35 +0300

    Disable csrf check for login view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
 from drf_spectacular.utils import extend_schema
 from rest_framework.exceptions import NotAuthenticated
 from rest_framework.request import Request
@@ -21,6 +22,7 @@ class UserLoginView(APIView):
     authentication_classes = ()
     permission_classes = ()
 
+    @csrf_exempt
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         serializer = UserLoginRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
