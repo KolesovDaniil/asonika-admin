d6d1fca7c20cbe48a12d7ae0036515c68ecf1fa0
Kolesov Daniil <d.kolesov@tinkoff.ru>
2022-04-16 21:59:08 +0300

    Refactor Category model

--- categories/models.py
+++ categories/models.py
@@ -1,22 +1,36 @@
 from uuid import uuid4
 
 from django.db import models
+from parameters.models import Parameter
+
+
+class ParamToCategorySettings(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid4)
+    parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    required = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = [('parameter', 'category')]
 
 
 class Category(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4)
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True)
+    parameters = models.ManyToManyField(
+        Parameter,
+        related_name='categories',
+        through=ParamToCategorySettings,
+    )
+    parent = models.ForeignKey(
+        'self',
+        verbose_name='Родительская категория',
+        related_name='children',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self) -> str:
-        return f'{self.name}'
-
-
-class ParamToCategorySettings(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid4)
-    param_uuid = models.UUIDField()
-    category_uuid = models.UUIDField()
-    required = models.BooleanField()
-
-    class Meta:
-        unique_together = [('param_uuid', 'category_uuid')]
+        return f'Category: {self.name}'
